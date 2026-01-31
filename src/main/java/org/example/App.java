870f9618171e8349a8b2d642e6b9265c86744a6b
eliasdlc <eliasdlc2005@gmail.com>
2026-01-30 21:31:53 -0400

    feat: Implement basic user authentication with login, logout, and protected routes.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,22 +1,43 @@
 package org.example;
+
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
 public class App {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 
+        var app = Javalin.create(config -> {
+            config.staticFiles.add("/public", Location.CLASSPATH);
+        }).start(7000);
 
-        Javalin app = Javalin.create(config -> {
-            config.addStaticFiles("/public", Location.CLASSPATH);
-         }).start(7000);
-
-        // Filtro
-
-         app.before(ctx -> {
-            if (!ctx.path().equals("/login.html")){
-
+        app.before(ctx -> {
+            if (!ctx.path().equals("/login.html") && !ctx.path().equals("/autenticar")
+                    && !ctx.path().endsWith(".css")) {
+                if (ctx.sessionAttribute("usuario") == null) {
+                    ctx.redirect("/login.html");
+                }
             }
-         });
+        });
+
+        app.post("/autenticar", ctx -> {
+
+            String user = ctx.formParam("username");
+            String pass = ctx.formParam("password");
+
+            if ("admin".equals(user) && "1234".equals(pass)) {
+                ctx.sessionAttribute("usuario", user);
+
+                ctx.redirect("/home.html");
+            } else {
+                ctx.redirect("/login.html");
+            }
+        });
+
+        app.get("/logout", ctx -> {
+            ctx.sessionAttribute("usuario", null);
+            ctx.redirect("/login.html");
+        });
+
     }
 
 }
